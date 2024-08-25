344ab30468adf46a25ec63053087dafc67e677c4
Anand Sahni <anandsahni57@gmail.com>
2024-08-25 10:48:30 +0530

    changed the file from python to js

---
+++ testfile.js
@@ -0,0 +1,11 @@
+let n = 10;
+let num1 = 0;
+let num2 = 1;
+let next_number = num2;
+let count = 1;
+while (count <= n) {
+  console.log(next_number, " ");
+  count++;
+  [num1, num2] = [num2, next_number];
+  next_number = num1 + num2;
+}
